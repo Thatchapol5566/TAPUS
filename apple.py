223072c7511bf150bec18addac590e5c9aa7fba2
Thatchapol5566 <thatchapol123456@gmail.com>
2025-01-20 10:27:20 +0700

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,72 @@
+#logo apple
+import turtle
+t=turtle.Turtle()
+
+# t.speed(8)
+t.pencolor('black')
+t.begin_fill()
+t.fillcolor("black")
+
+t.left(138)
+for i in range(35):
+    t.fd(1)
+    t.left(1)
+t.right(5)
+for i in range(35):
+    t.fd(1)
+    t.left(1)
+t.right(5)
+t.fd(30)
+
+for i in range(15):
+    t.fd(0.7)
+    t.right(3)
+t.fd(25)
+t.left(5)
+
+for i in range(50):
+    t.fd(1)
+    t.left(1)
+    t.right
+t.right(3)
+
+for i in range(50):
+    t.fd(1)
+    t.left(1)
+t.right(5)
+
+for i in range(45):
+    t.fd(2)
+    t.left(1)
+t.right(5)
+
+for i in range(45):
+    t.fd(2)
+    t.left(1)
+t.right(5)
+#ส่วนล่างของผล
+for i in range(20):
+    t.fd(1)
+    t.left(2)
+t.left(5)
+t.fd(25)
+
+for i in range(8):
+    t.fd(2)
+    t.left(3)
+t.fd(1)
+
+for i in range(5):
+    t.fd(2)
+    t.left(1)
+t.right(4)
+t.fd(4.5)
+t.right(2)
+for i in range(25):
+    t.fd(1)
+    t.left(2)
+t.left(8)
+t.fd(10)
+
+
+turtle.done()
